e9e8019f22ae27d7cf6dd96c1459e9eece3460a8
AbduSomad <abdusomadkoshmamatov@gmail.com>
2023-02-10 23:31:39 +0600

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //todo Бул айлананын аянтын жана узундугун тапкан методтор
         Circle.area(10);
         Circle.circumference(15);
     }
